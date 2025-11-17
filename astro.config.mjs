6624c2fe33ef4f955589584b31732ab8a30c22ce
Yann Duval <yann.duval@posteo.de>
2025-11-17 09:53:19 +0100

    First links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://devndive.github.io',
-	base: '/links',
+  site: 'https://devndive.github.io',
+  base: '/links',
 });
